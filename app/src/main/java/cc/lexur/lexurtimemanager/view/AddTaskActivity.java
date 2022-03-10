057868a21d342a4fdf379d5780af1c8780656d48
lexur <2259334682@qq.com>
2022-03-10 12:35:50 +0800

    创建添加任务页面

--- app/src/main/java/cc/lexur/lexurtimemanager/view/AddTaskActivity.java
+++ app/src/main/java/cc/lexur/lexurtimemanager/view/AddTaskActivity.java
@@ -1,16 +1,35 @@
 package cc.lexur.lexurtimemanager.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.SavedStateViewModelFactory;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
 import cc.lexur.lexurtimemanager.R;
+import cc.lexur.lexurtimemanager.TaskViewModel;
+import cc.lexur.lexurtimemanager.databinding.ActivityAddTaskBinding;
 
 public class AddTaskActivity extends AppCompatActivity {
+
+    ActivityAddTaskBinding binding;
+    TaskViewModel taskViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_task);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_add_task);
+        taskViewModel = new ViewModelProvider(this, new SavedStateViewModelFactory(getApplication(),this)).get(TaskViewModel.class);
+        binding.setLifecycleOwner(this);
+
+        init();
+    }
+
+    /**
+     * 初始化
+     */
+    private void init(){
+
     }
 }
