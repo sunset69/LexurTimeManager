41691649d9eb33e0e7870df65d4f2129eb6b95d6
lexur <2259334682@qq.com>
2022-03-11 23:16:45 +0800

    完善Task

--- app/src/main/java/cc/lexur/lexurtimemanager/room/Task.java
+++ app/src/main/java/cc/lexur/lexurtimemanager/room/Task.java
@@ -6,6 +6,7 @@ import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
 import java.util.Date;
+import java.util.jar.Attributes;
 
 @Entity
 public class Task {
@@ -14,15 +15,39 @@ public class Task {
     int id;
 
     private String name;
-    private String description;
 
+    private String description;
 
     @ColumnInfo(name = "create_time")
     private Date createTime;
 
-    @Ignore
-    public Task() {
+    private boolean status;
 
+    @ColumnInfo(name = "label_id")
+    private int labelId;
+
+    @ColumnInfo(name = "start_time")
+    private Date startTime;
+
+    @ColumnInfo(name = "stop_time")
+    private Date stopTime;
+
+    @ColumnInfo(name = "priority", defaultValue = "0")
+    private int priority;
+
+    @ColumnInfo(name = "is_archive", defaultValue = "false")
+    private boolean isArchive;
+
+    @ColumnInfo(name = "archive_time")
+    private boolean archiveTime;
+
+    public Task() {
+    }
+
+    @Ignore
+    public Task(String name, String description) {
+        this.name = name;
+        this.description = description;
     }
 
     @Override
@@ -32,20 +57,14 @@ public class Task {
                 ", name='" + name + '\'' +
                 ", description='" + description + '\'' +
                 ", createTime=" + createTime +
+                ", status=" + status +
+                ", labelId=" + labelId +
+                ", startTime=" + startTime +
+                ", stopTime=" + stopTime +
+                ", priority=" + priority +
+                ", isArchive=" + isArchive +
+                ", archiveTime=" + archiveTime +
                 '}';
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Task(String name, String description) {
-        this.name = name;
-        this.description = description;
     }
 
     public int getId() {
@@ -70,5 +89,69 @@ public class Task {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public int getLabelId() {
+        return labelId;
+    }
+
+    public void setLabelId(int labelId) {
+        this.labelId = labelId;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getStopTime() {
+        return stopTime;
+    }
+
+    public void setStopTime(Date stopTime) {
+        this.stopTime = stopTime;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public boolean isArchive() {
+        return isArchive;
+    }
+
+    public void setArchive(boolean archive) {
+        isArchive = archive;
+    }
+
+    public boolean isArchiveTime() {
+        return archiveTime;
+    }
+
+    public void setArchiveTime(boolean archiveTime) {
+        this.archiveTime = archiveTime;
     }
 }
